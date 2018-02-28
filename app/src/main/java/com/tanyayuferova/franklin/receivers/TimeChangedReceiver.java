1ae3dbec58fcf0b6601f2f734e3f9ec552e9942e
Tanya Yuferova <tanya_yu_95@mail.ru>
2018-02-28 22:00:06 +0500

    Fix error start service in 8.0

--- app/src/main/java/com/tanyayuferova/franklin/receivers/TimeChangedReceiver.java
+++ app/src/main/java/com/tanyayuferova/franklin/receivers/TimeChangedReceiver.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.tanyayuferova.franklin.services.TimeChangeIntentService;
+import com.tanyayuferova.franklin.utils.EveryDayReminderUtils;
 
 /**
  * Receives time and time zone device change
@@ -18,7 +18,7 @@ public class TimeChangedReceiver extends BroadcastReceiver {
         if(Intent.ACTION_TIME_CHANGED.equals(intent.getAction()) ||
                 Intent.ACTION_TIMEZONE_CHANGED.equals(intent.getAction())) {
             //We need to update virtues values in the data base according to current device localization
-            context.startService(new Intent(context, TimeChangeIntentService.class));
+            EveryDayReminderUtils.scheduleStartReminderJob(context);
         }
     }
 }
